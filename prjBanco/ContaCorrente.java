9cdc286a9592ea132b92dccfdff54437061f3a46
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-12 16:24:14 -0300

    Adiciona classes ContaCorrente e ContaPoupanca - exercicio 3

---
+++ prjBanco/ContaCorrente.java
@@ -0,0 +1,5 @@
+class ContaCorrente extends Conta {
+    public void atualiza(double taxa) {
+	this.saldo *= (1 + 2 * taxa);
+    }
+}
---
+++ prjBanco/ContaPoupanca.java
@@ -0,0 +1,9 @@
+class ContaPoupanca extends Conta {
+    public void atualiza(double taxa) {
+	this.saldo *= (1 + 3 * taxa);
+    }
+
+    public void deposita(double valor) {
+	this.saldo += valor - 0.10;
+    }
+}
