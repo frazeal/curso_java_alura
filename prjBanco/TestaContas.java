a06a169c700c79832775cd00751af356191da892
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-12 16:33:12 -0300

    Adiciona polimorfismo na classe TestaContas

--- prjBanco/TestaContas.java
+++ prjBanco/TestaContas.java
@@ -1,8 +1,8 @@
 class TestaContas {
     public static void main(String[] args) {
 	Conta c = new Conta();
-	ContaCorrente cc = new ContaCorrente();
-	ContaPoupanca cp = new ContaPoupanca();
+	Conta cc = new ContaCorrente();
+	Conta cp = new ContaPoupanca();
 
 	c.deposita(1000);
 	cc.deposita(1000);
