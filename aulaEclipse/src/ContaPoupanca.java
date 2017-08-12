e2a94350a22683dac4211d935364a0b2ecfb3884
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-12 18:43:40 -0300

    Formata pelo eclipse o fonte das classes Principal e ContaCorrente - exercicio 5

--- aulaEclipse/src/ContaPoupanca.java
+++ aulaEclipse/src/ContaPoupanca.java
@@ -3,7 +3,7 @@ public class ContaPoupanca extends Conta {
 	public void atualiza(double taxa) {
 		this.saldo *= (1 + 3 * taxa);
 	}
-	
+
 	public void deposita(double valor) {
 		this.saldo += valor - 0.10;
 	}
