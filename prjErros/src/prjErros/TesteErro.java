93c03e90ddda1b1018f1496f75d67ad14a4f5d96
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-20 17:51:54 -0300

    Adiciona o exercicio 5

--- prjErros/src/prjErros/TesteErro.java
+++ prjErros/src/prjErros/TesteErro.java
@@ -2,18 +2,18 @@ package prjErros;
 
 public class TesteErro {
 	public static void main(String[] args) {
-		System.out.println("inicio do main");
-		metodo1();
-		System.out.println("fim do main");
+	    System.out.println("inicio do main");
+	    try {
+	        metodo1();
+	    } catch (ArrayIndexOutOfBoundsException e) {
+	        System.out.println("Erro : "+e);
+	    }
+	    System.out.println("fim do main");
 	}
 
 	public static void metodo1() {
 		System.out.println("inicio do metodo1");
-		try {
-			metodo2();
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("erro: " + e);
-		}
+		metodo2();
 		System.out.println("fim do metodo1");
 	}
 
