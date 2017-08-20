cb05b4fbf65bbc79f4a3ec008b2a722d63796119
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-20 16:36:50 -0300

    Adiciona o exercício 2

--- prjBanco/ContaCorrente.java
+++ prjBanco/ContaCorrente.java
@@ -1,5 +1,10 @@
-class ContaCorrente extends Conta {
+class ContaCorrente extends Conta implements Tributavel {
     public void atualiza(double taxa) {
 	this.saldo *= (1 + 2 * taxa);
     }
+
+	@Override
+	public double calcularTributos() {
+		return this.getSaldo() * 0.01;
+	}
 }
---
+++ prjBanco/SeguroVida.java
@@ -0,0 +1,9 @@
+
+public class SeguroVida implements Tributavel {
+
+	@Override
+	public double calcularTributos() {
+		return 42;
+	}
+
+}
