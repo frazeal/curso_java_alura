c2cb2d64eb824fa7874786766399a3929db73dc1
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-20 18:01:34 -0300

    Adiciona o exercicio 8

--- aulaEclipse/src/Principal.java
+++ aulaEclipse/src/Principal.java
@@ -3,9 +3,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Conta conta = new ContaCorrente();
-		conta.deposita(-100.0);
-		System.out.println(conta.getSaldo());
-
+		try {
+			conta.deposita(-100.0);
+			System.out.println(conta.getSaldo());
+		} catch (IllegalArgumentException ie) {
+			System.out.println("Você tentou depositar um valor inválido");
+		}
 		Conta contaCorrente = new ContaCorrente();
 		contaCorrente.atualiza(0.02);
 	}
