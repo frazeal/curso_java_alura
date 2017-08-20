e15cb36da768a13f4e8762244555d2a78c39186d
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-20 17:41:07 -0300

    Adiciona exercício 2

--- prjErros/src/prjErros/TesteErro.java
+++ prjErros/src/prjErros/TesteErro.java
@@ -16,9 +16,13 @@ public class TesteErro {
     public static void metodo2() {
         System.out.println("inicio do metodo2");
         int[] array = new int[10];
-        for (int i = 0; i <= 15; i++) {
-            array[i] = i;            
-            System.out.println(i);
+        try {
+	        for (int i = 0; i <= 15; i++) {
+	            array[i] = i;            
+	            System.out.println(i);
+	        }
+        } catch (Exception e) {
+        	System.out.println("Erro: " + e.toString());
         }
         System.out.println("fim do metodo2");
     }
