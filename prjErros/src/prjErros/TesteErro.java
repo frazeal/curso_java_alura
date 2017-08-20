d121d55ca135a47cb9a79d6a6d0d745ad85fa65d
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-20 17:50:13 -0300

    Adiciona o exercicio 4

--- prjErros/src/prjErros/TesteErro.java
+++ prjErros/src/prjErros/TesteErro.java
@@ -1,29 +1,29 @@
 package prjErros;
 
 public class TesteErro {
-    public static void main(String[] args) {
-        System.out.println("inicio do main");
-        metodo1();
-        System.out.println("fim do main");
-    }
+	public static void main(String[] args) {
+		System.out.println("inicio do main");
+		metodo1();
+		System.out.println("fim do main");
+	}
 
-    public static void metodo1() {
-        System.out.println("inicio do metodo1");
-        metodo2();
-        System.out.println("fim do metodo1");
-    }
+	public static void metodo1() {
+		System.out.println("inicio do metodo1");
+		try {
+			metodo2();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("erro: " + e);
+		}
+		System.out.println("fim do metodo1");
+	}
 
-    public static void metodo2() {
-        System.out.println("inicio do metodo2");
-        int[] array = new int[10];
-        for (int i = 0; i <= 15; i++) {
-            try {
-                array[i] = i;            
-                System.out.println(i);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("erro: " + e);
-            }
-        }
-        System.out.println("fim do metodo2");
-    }
+	public static void metodo2() {
+		System.out.println("inicio do metodo2");
+		int[] array = new int[10];
+		for (int i = 0; i <= 15; i++) {
+			array[i] = i;
+			System.out.println(i);
+		}
+		System.out.println("fim do metodo2");
+	}
 }
