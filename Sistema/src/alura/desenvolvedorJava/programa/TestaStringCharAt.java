93668f907a6b7fe53a4f74d15024352b25f1e936
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-08-27 19:47:37 -0300

    Adiciona o exercício 12

--- Sistema/src/alura/desenvolvedorJava/programa/TestaStringCharAt.java
+++ Sistema/src/alura/desenvolvedorJava/programa/TestaStringCharAt.java
@@ -6,10 +6,18 @@ public class TestaStringCharAt {
 		// TODO Auto-generated method stub
 		String teste = "Vamos testar o funcionamento do método criado.";
 		imprimeLetraPorLetra(teste);
+		imprimeLetraPorLetraReverso("Socorram-me subi no ônibus em marrocos");
+		imprimeLetraPorLetraReverso("Anotaram a data da maratona");
 	}
 	
 	public static void imprimeLetraPorLetra(String st) {
 		for(int i = 0; i < st.length(); i++) {
+			System.out.println(st.charAt(i));
+		}
+	}
+	
+	public static void imprimeLetraPorLetraReverso(String st) {
+		for (int i = st.length() - 1; i >= 0; i--) {
 			System.out.println(st.charAt(i));
 		}
 	}
